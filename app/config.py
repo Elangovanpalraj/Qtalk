5999bace2f963600fe25d3f72f369c1deb4d8854
ELANGOVAN PALRAJ <167401951+Elangovanpalraj@users.noreply.github.com>
2026-08-07 13:56:03 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,7 @@
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./quantum_connect.db")
+# Database and Security Configurations for Qtalk
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./qtalk.db")
 SECRET_KEY = os.getenv("SECRET_KEY", "quantum_super_secret_key_123")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
